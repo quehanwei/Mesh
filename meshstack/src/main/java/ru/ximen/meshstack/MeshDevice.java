de3345ae2ef915495188af31584dffc6385968c2
ximen <ximen@ximen.ru>
2018-07-15 19:58:34 +0300

    Added app keys to MeshDevice

--- meshstack/src/main/java/ru/ximen/meshstack/MeshDevice.java
+++ meshstack/src/main/java/ru/ximen/meshstack/MeshDevice.java
@@ -6,6 +6,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -19,9 +21,11 @@ public class MeshDevice {
     private byte[] mDeviceKey;
     private UUID mUUID;
     private boolean mProxy = true;
+    private HashMap<Byte, byte[]> models;
     //private BluetoothDevice mBluetoothDevice;
 
     public MeshDevice(JSONObject json) {
+        models = new HashMap<>();
         try {
             mAddress = (short) json.getInt("address");
             mMAC = json.getString("mac");
@@ -30,6 +34,13 @@ public class MeshDevice {
             JSONArray features = json.getJSONArray("features");
             for (int i = 0; i < features.length(); i++) {
                 if (features.getString(i).equals("proxy")) mProxy = true;
+            }
+            JSONArray mod = json.getJSONArray("models");
+            for (int i = 0; i < mod.length(); i++) {
+                JSONObject obj = mod.getJSONObject(i);
+                byte AID = (byte)obj.getInt("aid");
+                String m = obj.getString("model");
+                models.put(AID, Utils.hexString2Bytes(m));
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -56,6 +67,14 @@ public class MeshDevice {
             JSONArray features = new JSONArray();
             if (isProxy()) features.put("proxy");
             json.put("features", features);
+            JSONArray mod = new JSONArray();
+            for (Map.Entry<Byte, byte[]> entry : models.entrySet()) {
+                JSONObject obj = new JSONObject();
+                obj.put("model", Utils.toHexString(entry.getValue()));
+                obj.put("aid", entry.getKey());
+                mod.put(obj);
+            }
+            json.put("models", mod);
         } catch (JSONException e) {
             e.printStackTrace();
         }
